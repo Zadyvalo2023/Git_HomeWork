1c9f8b890cdd43c31c44647f34f1497de3c5edbd
Daniil Pilipenko <sortedmap@gmail.com>
2022-10-31 22:48:16 +0300

    adding result field instead of alert

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
     let textArea = document.querySelector('#text');
     let button = document.querySelector('#count-button');
+    let result = document.querySelector('#result');
     button.addEventListener('click', function(){
         let text = textArea.value;
-        alert('Длина текста: ' + text.length + ' символов');
+        result.innerHTML = '<b>Длина текста: </b>' +
+            text.length + ' символов';
     });
 });
